7229a1df532fbe1f448f807d0c76f37ea7c429e5
joycebinu <binu_mat@yahoo.com>
2018-07-28 13:32:24 -0400

    code for assignment 2

--- Project2/main.c
+++ Project2/main.c
@@ -6,23 +6,79 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <string.h>
 #include <math.h>
-#include <sys/wait.h>
 #include <pthread.h>
 
-int start_points[4];
-double grand_sum;
-int grand_sum_lock;
+#define ARR_LENGTH 4
 
+double *grand_sum_ptr;
+pthread_mutex_t grand_sum_lock;
 
+//critical region update function
+void add_all(double t)
+{
+   pthread_mutex_lock(&grand_sum_lock);   //acquiring lock
+   *grand_sum_ptr += t;                   //updating grand_sum
+   pthread_mutex_unlock(&grand_sum_lock); //lock release
+}
 
-int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
-    return 0;
+void *sum_of_sqrt(void *arr)
+{
+  int i;
+  double sum = 0.0;
+  int *num = (int *)arr;   //retrieving the array values to use
+  
+  for (i=*num; i<*(num+1); i++)
+  {
+    sum += sqrt(i);
+  }
+  add_all(sum);            //function to update the critical region
+}
 
-
-
+int main(int argc, char *argv[])
+{
+  double grand_sum = 0;
+  int start_points[ARR_LENGTH], n, i, j;
+  pthread_t threads[2];
+  grand_sum_ptr = &grand_sum;
+  
+  pthread_mutex_init(&grand_sum_lock, 0);  //mutex initialization
+  
+  printf("Enter any number (Should be a multiple of three): ");
+  scanf("%d", &n);
+  
+  if (n%3 != 0)
+  {
+    printf("Invalid entry!\n");
+    exit(1);
+  }
+  else
+  {
+    for (i=0; i<ARR_LENGTH; i++)       //loop to update start_points
+    {
+      start_points[i] = (i*(n/3))+1;
+      //printf("start_points[%d] = %d \n", i, start_points[i]);
+    }
+    j=1;                               //counter for start_point  postions
+    for (i=0; i<2; i++)
+    {
+      pthread_create(&threads[i], NULL, sum_of_sqrt, (void *)&start_points[j]);        //child thread creation
+      j++;
+    }
+      
+    sum_of_sqrt((void *)&start_points[0]);     //main thread's call to sum_of_sqrt()
+    
+    for (i=0; i<2; i++)
+    {
+      pthread_join(threads[i], NULL);    //waiting for all child threads to finish
+    }
+    printf("sum of square roots: %lf \n", grand_sum);
+    pthread_mutex_destroy(&grand_sum_lock);
+  }
+  
+    
+  
+  
+  
+  
 }
