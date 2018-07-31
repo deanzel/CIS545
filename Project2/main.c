7ae66137d4fea328c6db8c46d3d057e08a7a5b1d
deanzel <deanchoi@gmail.com>
2018-07-30 23:02:45 -0400

    Changed makefiles and fixed Assignment 2 format to read input directly from command line

--- Project2/main.c
+++ Project2/main.c
@@ -8,6 +8,8 @@
 #include <stdlib.h>
 #include <math.h>
 #include <pthread.h>
+#include <ctype.h>
+#include <string.h>
 
 #define ARR_LENGTH 4
 
@@ -15,15 +17,13 @@ double *grand_sum_ptr;
 pthread_mutex_t grand_sum_lock;
 
 //critical region update function
-void add_all(double t)
-{
+void add_all(double t) {
    pthread_mutex_lock(&grand_sum_lock);   //acquiring lock
    *grand_sum_ptr += t;                   //updating grand_sum
    pthread_mutex_unlock(&grand_sum_lock); //lock release
 }
 
-void *sum_of_sqrt(void *arr)
-{
+void *sum_of_sqrt(void *arr) {
   int i;
   double sum = 0.0;
   int *num = (int *)arr;   //retrieving the array values to use
@@ -35,50 +35,74 @@ void *sum_of_sqrt(void *arr)
   add_all(sum);            //function to update the critical region
 }
 
-int main(int argc, char *argv[])
-{
-  double grand_sum = 0;
-  int start_points[ARR_LENGTH], n, i, j;
-  pthread_t threads[2];
-  grand_sum_ptr = &grand_sum;
-  
-  pthread_mutex_init(&grand_sum_lock, 0);  //mutex initialization
-  
-  printf("Enter any number (Should be a multiple of three): ");
-  scanf("%d", &n);
-  
-  if (n%3 != 0)
-  {
-    printf("Invalid entry!\n");
-    exit(1);
-  }
-  else
-  {
-    for (i=0; i<ARR_LENGTH; i++)       //loop to update start_points
-    {
-      start_points[i] = (i*(n/3))+1;
-      //printf("start_points[%d] = %d \n", i, start_points[i]);
+
+//Make it a command line input
+int main(int argc, char *argv[]) {
+
+    if (argc != 2){     //Have more than 2 arguments (or only 1) in command line call
+        printf("\nThat is the incorrect number of command line arguments...\n");
+        exit(1);
+
     }
-    j=1;                               //counter for start_point  postions
-    for (i=0; i<2; i++)
-    {
-      pthread_create(&threads[i], NULL, sum_of_sqrt, (void *)&start_points[j]);        //child thread creation
-      j++;
+    else {      //Have exactly 2 arguments
+        //Check if the 2nd argument string is an integer
+        for (int i=0; i < strlen(argv[1]); i++) {
+            if (!isdigit(argv[1][i])) {     //Checks each char to see if it is numeric digit; if not, error exit
+                printf("\nThe command line input is not a number...\n");
+                exit(2);
+            }
+        }
+
+        //2nd argument is a number so convert that string into an integer n
+        int n = atoi(argv[1]);
+
+        if (n%3 != 0)
+        {
+            printf("\nThe input number is not a multiple of 3...\n");
+            exit(3);
+        }
+        else
+        {
+
+            printf("%d is the number!!!!!", n);
+
+            double grand_sum = 0;
+            int start_points[ARR_LENGTH], n, i, j;
+            pthread_t threads[2];
+            grand_sum_ptr = &grand_sum;
+
+            pthread_mutex_init(&grand_sum_lock, 0);  //mutex initialization
+
+            for (i=0; i<ARR_LENGTH; i++)       //loop to update start_points
+            {
+                start_points[i] = (i*(n/3))+1;
+                //printf("start_points[%d] = %d \n", i, start_points[i]);
+            }
+            j=1;                               //counter for start_point  postions
+            for (i=0; i<2; i++)
+            {
+                pthread_create(&threads[i], NULL, sum_of_sqrt, (void *)&start_points[j]);        //child thread creation
+                j++;
+            }
+
+            sum_of_sqrt((void *)&start_points[0]);     //main thread's call to sum_of_sqrt()
+
+            for (i=0; i<2; i++)
+            {
+                pthread_join(threads[i], NULL);    //waiting for all child threads to finish
+            }
+            printf("\nThe sum of the square roots: %lf \n", grand_sum);
+            pthread_mutex_destroy(&grand_sum_lock);
+        }
+
+
+
+
     }
-      
-    sum_of_sqrt((void *)&start_points[0]);     //main thread's call to sum_of_sqrt()
-    
-    for (i=0; i<2; i++)
-    {
-      pthread_join(threads[i], NULL);    //waiting for all child threads to finish
-    }
-    printf("sum of square roots: %lf \n", grand_sum);
-    pthread_mutex_destroy(&grand_sum_lock);
-  }
-  
-    
-  
-  
+
+
+
+  return 0;
   
   
 }
